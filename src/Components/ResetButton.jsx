8b1d59837b4cb7dc47e9be3927b365d9901acf44
f.samuel <samuelf8908@gmail.com>
2024-05-22 01:14:13 -0700

    first commit

---
+++ src/Components/ResetButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./ResetButton.css"
+
+
+const ResetButton = ({ resetBoard}) => {
+  return (
+
+   <button className='reset-btn' onClick={resetBoard}>Reset</button>
+
+  )
+}
+
+export default ResetButton
